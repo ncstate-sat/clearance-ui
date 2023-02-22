143b14413d2d98fab7e3848db6177c12dc99c60d
John Champion <jtchampi@ncsu.edu>
2023-02-22 14:21:36 -0500

    fix tag input bugs in assign page

--- src/pages/Assign.jsx
+++ src/pages/Assign.jsx
@@ -1,124 +1,136 @@
 import {
-    Heading,
-    minorScale,
-    TagInput,
-    Text,
-    Button,
-    toaster,
-  } from 'evergreen-ui'
-  import { useMemo, useState, useEffect } from 'react'
-  import ContentCard from '../components/ContentCard'
-  import Layout from '../components/Layout'
-  
-  import useClearance from '../hooks/useClearance'
-  import usePersonnel from '../hooks/usePersonnel'
-  
-  import clearanceService from '../apis/clearanceService'
-  
-  export default function AssignClearance() {
-    const [assignClearance, { isLoading, isSuccess, isError, data }] =
-      clearanceService.useAssignClearancesMutation()
-  
-    const [selectedClearances, setSelectedClearances] = useState([])
-    const { clearances, setClearanceQuery } = useClearance()
+  Heading,
+  minorScale,
+  TagInput,
+  Text,
+  Button,
+  toaster,
+} from 'evergreen-ui'
+import { useMemo, useState, useEffect } from 'react'
+import ContentCard from '../components/ContentCard'
+import Layout from '../components/Layout'
+
+import useClearance from '../hooks/useClearance'
+import usePersonnel from '../hooks/usePersonnel'
+
+import clearanceService from '../apis/clearanceService'
+
+export default function AssignClearance() {
+  const [assignClearance, { isLoading, isSuccess, isError, data }] =
+    clearanceService.useAssignClearancesMutation()
+
+  const [selectedClearances, setSelectedClearances] = useState([])
+  const { clearances, setClearanceQuery } = useClearance()
+
+  const [selectedPersonnel, setSelectedPersonnel] = useState([])
+  const { personnel, setPersonnelQuery } = usePersonnel()
+
+  // const [startDate, setStartDate] = useState(null)
+  // const [endDate, setEndDate] = useState(null)
+
+  // Suggestion strings for clearances.
+  const autocompleteClearances = useMemo(() => {
     const clearanceNames = clearances.map((c) => c['name'])
-  
-    const [selectedPersonnel, setSelectedPersonnel] = useState([])
-    const { personnel, setPersonnelQuery } = usePersonnel()
+    const selectedClearanceNames = selectedClearances.map((c) => c['name'])
+    return clearanceNames.filter((i) => !selectedClearanceNames.includes(i))
+  }, [clearances, selectedClearances])
+
+  // Suggestion strings for personnel.
+  const autocompletePersonnel = useMemo(() => {
     const personnelStrings = personnel.map(
       (p) =>
         `${p['first_name']} ${p['last_name']} (${p['email']}) [${p['campus_id']}]`
     )
-  
-    // const [startDate, setStartDate] = useState(null)
-    // const [endDate, setEndDate] = useState(null)
-  
-    const autocompleteClearances = useMemo(
-      () => clearanceNames.filter((i) => !selectedClearances.includes(i)),
-      [clearanceNames, selectedClearances]
+    const selectedPersonnelStrings = selectedPersonnel.map(
+      (p) =>
+        `${p['first_name']} ${p['last_name']} (${p['email']}) [${p['campus_id']}]`
     )
-  
-    const autocompletePersonnel = useMemo(
-      () => personnelStrings.filter((i) => !selectedPersonnel.includes(i)),
-      [personnelStrings, selectedPersonnel]
-    )
-  
-    const getPersonnel = () => {
-      let personnelObjects = []
-      personnel.forEach((p) => {
-        const personString = `${p['first_name']} ${p['last_name']} (${p['email']}) [${p['campus_id']}]`
-        if (selectedPersonnel.includes(personString)) {
-          personnelObjects.push(p)
-        }
-      })
-      return personnelObjects
+    return personnelStrings.filter((i) => !selectedPersonnelStrings.includes(i))
+  }, [personnel, selectedPersonnel])
+
+  // Handle response from Assign call.
+  useEffect(() => {
+    if (isSuccess) {
+      setSelectedPersonnel([])
+      setSelectedClearances([])
+      toaster.success('Request Submitted')
+    } else if (isError) {
+      toaster.danger('Request Failed')
     }
-  
-    const getClearances = () => {
-      let clearanceObjects = []
-      clearances.forEach((c) => {
-        if (selectedClearances.includes(c['name'])) {
-          clearanceObjects.push(c)
-        }
-      })
-      return clearanceObjects
-    }
-  
-    useEffect(() => {
-      if (isSuccess) {
-        setSelectedPersonnel([])
-        setSelectedClearances([])
-        toaster.success('Request Submitted')
-      } else if (isError) {
-        toaster.danger('Request Failed')
-      }
-    }, [isSuccess, isError, data])
-  
-    const submitRequestHandler = async () => {
-      const selectedPersonnelObjects = getPersonnel()
-      const selectedClearanceObjects = getClearances()
-      const assigneeIds = selectedPersonnelObjects.map((p) => p['campus_id'])
-      const clearanceIds = selectedClearanceObjects.map((c) => c['id'])
-  
-      assignClearance({ assigneeIDs: assigneeIds, clearanceIDs: clearanceIds })
-    }
-  
-    return (
-      <Layout title='Assign'>
-        <Heading size={800}>Assign Clearances</Heading>
-        <Text>Add clearances to an individual</Text>
-  
-        <ContentCard>
-          <Heading size={600} marginBottom={minorScale(3)}>
-            Select Person
-          </Heading>
-          <TagInput
-            tagSubmitKey='enter'
-            width='100%'
-            values={selectedPersonnel}
-            onChange={setSelectedPersonnel}
-            autocompleteItems={autocompletePersonnel}
-            onInputChange={(e) => setPersonnelQuery(e.target.value)}
-            test-id='personnel-input'
-          />
-        </ContentCard>
-  
-        <ContentCard>
-          <Heading size={600} marginBottom={minorScale(3)}>
-            Select Clearance
-          </Heading>
-          <TagInput
-            tagSubmitKey='enter'
-            width='100%'
-            values={selectedClearances}
-            onChange={setSelectedClearances}
-            autocompleteItems={autocompleteClearances}
-            onInputChange={(e) => setClearanceQuery(e.target.value)}
-            test-id='clearance-input'
-          />
-        </ContentCard>
-  
-        {/* <ContentCard>
+  }, [isSuccess, isError, data])
+
+  // Submit Assign request.
+  const submitRequestHandler = async () => {
+    const assigneeIds = selectedPersonnel.map((p) => p['campus_id'])
+    const clearanceIds = selectedClearances.map((c) => c['id'])
+
+    assignClearance({ assigneeIDs: assigneeIds, clearanceIDs: clearanceIds })
+  }
+
+  return (
+    <Layout title='Assign'>
+      <Heading size={800}>Assign Clearances</Heading>
+      <Text>Add clearances to an individual</Text>
+
+      <ContentCard>
+        <Heading size={600} marginBottom={minorScale(3)}>
+          Select Person
+        </Heading>
+        <TagInput
+          tagSubmitKey='enter'
+          width='100%'
+          values={selectedPersonnel.map(
+            (p) =>
+              `${p['first_name']} ${p['last_name']} (${p['email']}) [${p['campus_id']}]`
+          )}
+          onChange={(selected) => {
+            const personnelObjects = []
+            const allPersonnel = [...personnel, ...selectedPersonnel]
+            const personnelStrings = allPersonnel.map(
+              (p) =>
+                `${p['first_name']} ${p['last_name']} (${p['email']}) [${p['campus_id']}]`
+            )
+            selected.forEach((s) => {
+              const i = personnelStrings.indexOf(s)
+              if (i >= 0) {
+                personnelObjects.push(allPersonnel[i])
+              }
+            })
+            setSelectedPersonnel(personnelObjects)
+          }}
+          autocompleteItems={autocompletePersonnel}
+          onInputChange={(e) => setPersonnelQuery(e.target.value)}
+          test-id='personnel-input'
+        />
+      </ContentCard>
+
+      <ContentCard>
+        <Heading size={600} marginBottom={minorScale(3)}>
+          Select Clearance
+        </Heading>
+        <TagInput
+          tagSubmitKey='enter'
+          width='100%'
+          values={selectedClearances.map((c) => c['name'])}
+          onChange={(selected) => {
+            const clearanceObjects = []
+            const allClearances = [...clearances, ...selectedClearances]
+            const clearanceStrings = allClearances.map((c) => `${c['name']}`)
+            selected.forEach((s) => {
+              const i = clearanceStrings.indexOf(s)
+              if (i >= 0) {
+                clearanceObjects.push(allClearances[i])
+              }
+            })
+            setSelectedClearances(clearanceObjects)
+          }}
+          autocompleteItems={autocompleteClearances}
+          onInputChange={(e) => setClearanceQuery(e.target.value)}
+          test-id='clearance-input'
+        />
+      </ContentCard>
+
+      {/* <ContentCard>
           <Heading size={600} marginBottom={minorScale(3)}>
             Set Start & End Times
           </Heading>
@@ -129,22 +141,21 @@ import {
             onChangeEndTime={setEndDate}
           />
         </ContentCard> */}
-  
-        <Button
-          appearance='primary'
-          intent='success'
-          isLoading={isLoading}
-          disabled={
-            selectedClearances.length === 0 || selectedPersonnel.length === 0
-          }
-          onClick={submitRequestHandler}
-          test-id='assign-clearance-btn'
-        >
-          Assign
-        </Button>
-      </Layout>
-    )
-  }
-  
-  AssignClearance.auth = true
-  
+
+      <Button
+        appearance='primary'
+        intent='success'
+        isLoading={isLoading}
+        disabled={
+          selectedClearances.length === 0 || selectedPersonnel.length === 0
+        }
+        onClick={submitRequestHandler}
+        test-id='assign-clearance-btn'
+      >
+        Assign
+      </Button>
+    </Layout>
+  )
+}
+
+AssignClearance.auth = true
