ae3785a30ffe03d3c7c7f302e68d67ce15faead1
John Champion <jtchampi@ncsu.edu>
2023-04-18 16:21:59 -0400

    add test to verify liaison assign and revoke requests work

--- e2e/clearance-manage.spec.js
+++ e2e/clearance-manage.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Assign & Manage Clearances page', () => {
+test.describe('[Admin] Assign & Manage Clearances page', () => {
   test.beforeEach(async ({ page }) => {
     const refreshToken = process.env['E2E_REFRESH_TOKEN']
 
@@ -40,5 +40,41 @@ test.describe('Assign & Manage Clearances page', () => {
     await expect(
       page.getByRole('heading', { name: 'Revoke Succeeded' })
     ).toBeVisible()
+  })
+})
+
+test.describe('[Liaison] Assign & Manage Clearances page', () => {
+  test.beforeEach(async ({ page }) => {
+    const refreshToken = process.env['E2E_LIAISON_REFRESH_TOKEN']
+
+    await page.addInitScript((rt) => {
+      window.localStorage.setItem('refresh-token', rt)
+    }, refreshToken)
+
+    await page.goto('/manage')
+  })
+
+  test('assign and revoke a clearance', async ({ page }) => {
+    await page.locator('[test-id="personnel-input"] input').fill('staylor8')
+    await page.getByText('Shawn Taylor (staylor8@ncsu.edu) [001120834]').click()
+
+    await page.locator('[test-id="clearance-input"] input').fill('ClearanceA')
+    await page.locator('//div[@id="TagInputAutocomplete-0-item-0"]').click()
+
+    await page.locator('[test-id=assign-clearance-btn]').click()
+
+    await expect(
+      page.getByRole('heading', { name: 'Clearance(s) Assigned Successfully' })
+    ).toBeVisible()
+
+    await page.locator('[test-id="revoke-clearance-btn"]').first().click()
+
+    await expect(
+      page.getByRole('heading', { name: 'Revoke Succeeded' })
+    ).toBeVisible()
+
+    await expect(
+      page.locator('[test-id="revoke-clearance-btn"]').first()
+    ).toBeDisabled()
   })
 })
