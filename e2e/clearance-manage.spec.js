bec2cdfe42303a9d72e6f58e0dee77c072f4ba6d
John Champion <jtchampi@ncsu.edu>
2023-05-02 09:44:13 -0400

    add tests

--- e2e/clearance-manage.spec.js
+++ e2e/clearance-manage.spec.js
@@ -41,6 +41,74 @@ test.describe('[Admin] Assign & Manage Clearances page', () => {
       page.getByRole('heading', { name: 'Revoke Succeeded' })
     ).toBeVisible()
   })
+
+  test('handle an error assigning a clearance', async ({ page }) => {
+    const ERROR_MESSAGE = '[TEST] Could not assign clearance.'
+
+    await page.route(/\/assignments\/assign$/, async (route) => {
+      await route.fulfill({
+        status: 500,
+        contentType: 'application/json',
+        body: JSON.stringify({ detail: ERROR_MESSAGE }),
+      })
+    })
+
+    await page.locator('[test-id="personnel-input"] input').fill('staylor8')
+    await page.getByText('Shawn Taylor (staylor8@ncsu.edu) [001120834]').click()
+
+    await page.locator('[test-id="clearance-input"] input').fill('ClearanceA')
+    await page.locator('//div[@id="TagInputAutocomplete-0-item-0"]').click()
+
+    await page.locator('[test-id=assign-clearance-btn]').click()
+
+    await expect(
+      page.getByRole('heading', { name: ERROR_MESSAGE })
+    ).toBeVisible()
+
+    const toastElement = await page.getByRole('heading', {
+      name: ERROR_MESSAGE,
+    })
+    const color = await toastElement.evaluate((el) =>
+      window.getComputedStyle(el).getPropertyValue('color')
+    )
+    expect(color).toBe('rgb(167, 54, 54)')
+  })
+
+  test('handle an error revoking a clearance assignment', async ({ page }) => {
+    const ERROR_MESSAGE = '[TEST] Could not revoke clearance assignment.'
+
+    await page.route(/\/assignments\/revoke$/, async (route) => {
+      await route.fulfill({
+        status: 500,
+        contentType: 'application/json',
+        body: JSON.stringify({ detail: ERROR_MESSAGE }),
+      })
+    })
+
+    await page.locator('[test-id="personnel-input"] input').fill('staylor8')
+    await page.getByText('Shawn Taylor (staylor8@ncsu.edu) [001120834]').click()
+
+    await page.locator('[test-id="clearance-input"] input').fill('ClearanceA')
+    await page.locator('//div[@id="TagInputAutocomplete-0-item-0"]').click()
+
+    await page.locator('[test-id=assign-clearance-btn]').click()
+
+    await expect(
+      page.getByRole('heading', { name: 'Clearance(s) Assigned Successfully' })
+    ).toBeVisible()
+
+    await page.locator('[test-id="revoke-clearance-btn"]').first().click()
+
+    const toastElement = await page.getByRole('heading', {
+      name: ERROR_MESSAGE,
+    })
+    const color = await toastElement.evaluate((el) =>
+      window.getComputedStyle(el).getPropertyValue('color')
+    )
+    expect(color).toBe('rgb(167, 54, 54)')
+
+    await page.unroute(/\/assignments\/revoke$/)
+  })
 })
 
 test.describe('[Liaison] Assign & Manage Clearances page', () => {
--- e2e/liaison-permissions.spec.js
+++ e2e/liaison-permissions.spec.js
@@ -1,34 +1,114 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test'
 
-test.describe("Liaison Permissions page", () => {
+test.describe('Liaison Permissions page', () => {
   test.beforeEach(async ({ page }) => {
-    const refreshToken = process.env["E2E_REFRESH_TOKEN"];
+    const refreshToken = process.env['E2E_REFRESH_TOKEN']
 
     await page.addInitScript((rt) => {
-      window.localStorage.setItem("refresh-token", rt);
-    }, refreshToken);
+      window.localStorage.setItem('refresh-token', rt)
+    }, refreshToken)
 
-    await page.goto("/liaison-permissions");
-  });
+    await page.goto('/liaison-permissions')
+  })
 
   // TODO: Remove John's info from E2E tests
-  test("view liaison permissions, add a permission, and remove a permission", async ({
+  test('view liaison permissions, add a permission, and remove a permission', async ({
     page,
   }) => {
-    await page.locator("#TagInput-2").fill("jtchampi");
+    await page.locator('#TagInput-2').fill('jtchampi')
     await page
-      .getByText("John Champion (jtchampi@ncsu.edu) [200103374]")
-      .click();
+      .getByText('John Champion (jtchampi@ncsu.edu) [200103374]')
+      .click()
 
-    await page.locator("#TagInput-4").fill("vrb");
-    await page.locator('//div[@id="TagInputAutocomplete-0-item-0"]').click();
+    await page.locator('#TagInput-4').fill('ClearanceA')
+    await page.locator('//div[@id="TagInputAutocomplete-0-item-0"]').click()
 
-    await page.locator("[test-id=assign-permission-btn]").click();
-
-    await page.locator("[test-id=revoke-permission-btn]").first().click();
+    await page.locator('[test-id=assign-permission-btn]').click()
 
     await expect(
-      page.getByRole("heading", { name: "Revoke Succeeded" })
-    ).toBeVisible();
-  });
-});
+      page.getByRole('heading', { name: 'Permissions Assigned' })
+    ).toBeVisible()
+
+    await page.locator('[test-id=revoke-permission-btn]').first().click()
+
+    await expect(
+      page.getByRole('heading', { name: 'Revoke Succeeded' })
+    ).toBeVisible()
+  })
+
+  // TODO: Remove John's info from E2E tests
+  test('handle an error assigning a permission', async ({ page }) => {
+    const ERROR_MESSAGE = '[TEST] Could not give permission.'
+
+    await page.route(/\/liaison\/assign$/, async (route) => {
+      await route.fulfill({
+        status: 500,
+        contentType: 'application/json',
+        body: JSON.stringify({ detail: ERROR_MESSAGE }),
+      })
+    })
+
+    await page.locator('#TagInput-2').fill('jtchampi')
+    await page
+      .getByText('John Champion (jtchampi@ncsu.edu) [200103374]')
+      .click()
+
+    await page.locator('#TagInput-4').fill('ClearanceA')
+    await page.locator('//div[@id="TagInputAutocomplete-0-item-0"]').click()
+
+    await page.locator('[test-id=assign-permission-btn]').click()
+
+    await expect(
+      page.getByRole('heading', { name: ERROR_MESSAGE })
+    ).toBeVisible()
+
+    const toastElement = await page.getByRole('heading', {
+      name: ERROR_MESSAGE,
+    })
+    const color = await toastElement.evaluate((el) =>
+      window.getComputedStyle(el).getPropertyValue('color')
+    )
+    expect(color).toBe('rgb(167, 54, 54)')
+  })
+
+  // TODO: Remove John's info from E2E tests
+  test('handle an error revoking a permission', async ({ page }) => {
+    const ERROR_MESSAGE = '[TEST] Could not give permission.'
+
+    await page.route(/\/liaison\/revoke$/, async (route) => {
+      await route.fulfill({
+        status: 500,
+        contentType: 'application/json',
+        body: JSON.stringify({ detail: ERROR_MESSAGE }),
+      })
+    })
+
+    await page.locator('#TagInput-2').fill('jtchampi')
+    await page
+      .getByText('John Champion (jtchampi@ncsu.edu) [200103374]')
+      .click()
+
+    await page.locator('#TagInput-4').fill('ClearanceA')
+    await page.locator('//div[@id="TagInputAutocomplete-0-item-0"]').click()
+
+    await page.locator('[test-id=assign-permission-btn]').click()
+
+    await expect(
+      page.getByRole('heading', { name: 'Permissions Assigned' })
+    ).toBeVisible()
+
+    await page.locator('[test-id=revoke-permission-btn]').first().click()
+
+    await expect(
+      page.getByRole('heading', { name: ERROR_MESSAGE })
+    ).toBeVisible()
+
+    const toastElement = await page.getByRole('heading', {
+      name: ERROR_MESSAGE,
+    })
+    const color = await toastElement.evaluate((el) =>
+      window.getComputedStyle(el).getPropertyValue('color')
+    )
+    expect(color).toBe('rgb(167, 54, 54)')
+  })
+})
