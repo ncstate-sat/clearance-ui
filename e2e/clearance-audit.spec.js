a9dafeea3e34fcd9ea508a212f0a884f3aa4c3be
Tanya <tdubey@ncsu.edu>
2023-06-01 09:40:34 -0400

    Added fix for broken test of the Audit Log screen

--- e2e/clearance-audit.spec.js
+++ e2e/clearance-audit.spec.js
@@ -2,18 +2,22 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Audit Log page', () => {
   test.beforeEach(async ({ page }) => {
-    const refreshToken = process.env['E2E_REFRESH_TOKEN']
+    // const refreshToken = process.env['E2E_REFRESH_TOKEN']
+    const refreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRkdWJleUBuY3N1LmVkdSIsImV4cCI6MTY4NTczMDg2NX0.weaFMzy4hjG5-SSeodDyFpqyNMeqbf3D8L6XDxTxuu4"
 
     await page.addInitScript((rt) => {
       window.localStorage.setItem('refresh-token', rt)
     }, refreshToken)
 
-    await page.goto('/audit')
+    await page.goto('https://clearance.test.ehps.ncsu.edu/audit', {waitUntil: "domcontentloaded"})
+    
   })
 
   test('it should display four columns and at least one row', async ({
     page,
   }) => {
+    await expect(page.getByRole('heading', { name: 'Audit Log' })).toBeVisible();
+    
     // Make sure we have four columns.
     const headerCells = await page.getByTestId('table-header').count()
     expect(headerCells).toBe(4)
