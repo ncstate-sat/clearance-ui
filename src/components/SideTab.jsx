8a6a39c94d232b6554cced8637a5ab7cf05aedf1
John Champion <johnt.champion@gmail.com>
2023-06-23 10:07:18 -0400

    set up pre-commit

--- src/components/SideTab.jsx
+++ src/components/SideTab.jsx
@@ -1,32 +1,24 @@
-import { minorScale, Tab } from "evergreen-ui";
+import { minorScale, Tab } from 'evergreen-ui'
 import { Link, useLocation } from 'react-router-dom'
 
-export default function SideTab({
-  title,
-  href = "#",
-  useAnchor = true
-}) {
+export default function SideTab({ title, href = '#', useAnchor = true }) {
   const location = useLocation()
 
   const tab = (
     <Tab
-      alignItems="flex-start"
-      direction="vertical"
-      appearance="secondary"
+      alignItems='flex-start'
+      direction='vertical'
+      appearance='secondary'
       isSelected={location.pathname === href}
       marginBottom={minorScale(2)}
     >
       {title}
     </Tab>
-  );
+  )
 
   if (useAnchor) {
-    return <a href={href}>{tab}</a>;
+    return <a href={href}>{tab}</a>
   } else {
-    return (
-      <Link to={href}>
-        {tab}
-      </Link>
-    );
+    return <Link to={href}>{tab}</Link>
   }
 }
