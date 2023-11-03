6a0b5a1236ce161fa321daf70e9ac5b1abc8aa60
Luc <lgsanche@ncsu.edu>
2023-11-03 13:54:13 -0400

    remove old tests

--- playwright/pages/AdminPage.js
+++ playwright/pages/AdminPage.js
@@ -1,5 +1,3 @@
-import { expect } from '@playwright/test'
-
 export class AdminPage {
     /**
      * @param {import('@playwright/test').Page} page
@@ -7,13 +5,55 @@ export class AdminPage {
     constructor(page) {
         this.page = page
         // reusable elements on the page
+        this.adminPersonInput = this.page.locator('[test-id="add-admin-input"] input')
+        this.addAdminButton = this.page.getByTestId('add-admin-btn')
+        this.addLiaisonButton = this.page.getByTestId('add-liaison-btn')
+        this.adminTable = this.page.getByTestId('admin-table')
+        this.liaisonTable = this.page.getByTestId('liaison-table')
+        // uses the automation email that was used to authenticate into the app
+        this.userRevokeButton = this.page.locator('[test-id="sat.admin.svc@ncsu.edu"] button')
+        this.pageHeader = this.page.getByRole('heading', { name: 'Manage Users' })
     }
 
     async goto() {
         await this.page.goto('/admin')
-        // Wait for all data to have loaded
-        await this.page.waitForLoadState('networkidle')
-        // const rowsBefore = await this.tableEntries.count()
-        // expect(rowsBefore).toBeGreaterThan(0)
+    }
+
+    async routePersonnelRequest() {
+        await this.page.route(/\/personnel\?search=fakep/, async (route) => {
+            await route.fulfill({
+                status: 200,
+                contentType: 'application/json',
+                body: JSON.stringify({
+                    personnel: [
+                        {
+                            first_name: 'Fake',
+                            middle_name: '',
+                            last_name: 'Person',
+                            email: 'fakep@ncsu.edu',
+                            campus_id: '000000000',
+                        },
+                    ],
+                }),
+            })
+        })
+    }
+
+    async unroutePersonnelRequest() {
+        await this.page.unroute(/\/people\?search=fakep/)
+    }
+
+    /**
+     * @param {string} text
+     */
+    async searchForPerson(text) {
+        await this.adminPersonInput.fill(text)
+    }
+
+    /**
+     * @param {string} selectorString
+     */
+    async clickOnPerson(selectorString) {
+        await this.page.locator(selectorString).click()
     }
 }
--- playwright/tests/admin/addAdmin.spec.js
+++ playwright/tests/admin/addAdmin.spec.js
@@ -1,49 +1,18 @@
 import { test, expect } from '@playwright/test'
 import { AdminPage } from '../../pages/AdminPage'
 
-// TODO: Remove John's info from E2E tests
 test('add an admin', async ({ page }) => {
     const adminPage = new AdminPage(page)
     await adminPage.goto()
 
-    await page.route(/\/personnel\?search=fakep/, async (route) => {
-        await route.fulfill({
-            status: 200,
-            contentType: 'application/json',
-            body: JSON.stringify({
-                personnel: [
-                    {
-                        first_name: 'Fake',
-                        middle_name: '',
-                        last_name: 'Person',
-                        email: 'fakep@ncsu.edu',
-                        campus_id: '001136721',
-                    },
-                ],
-            }),
-        })
-    })
+    await adminPage.routePersonnelRequest()
 
-    await page
-        .locator('[test-id="add-admin-input"] input')
-        .fill('fakep')
+    await adminPage.searchForPerson('fakep')
+    // this will click on the search result that pops up in the search bar, matching the the string after "text="
+    await adminPage.clickOnPerson('text=Fake Person (fakep@ncsu.edu) [000000000]')
+    await adminPage.addAdminButton.click()
 
-    await page
-        .locator('text=John Champion (jtchampi@ncsu.edu) [200103374]')
-        .click()
+    await expect(adminPage.adminTable).toContainText('fakep@ncsu.edu')
 
-
-
-    // await page
-    //     .locator('text=Luc Sanchez (lgsanche@ncsu.edu) [001136746]')
-    //     .click()
-
-    await page.locator('[test-id="add-admin-btn"]').click()
-
-    await expect(page.locator('[test-id="admin-table"]')).toContainText(
-        'jtchampi@ncsu.edu'
-    )
-
-    await page.unroute(/\/people\?search=lgsanche/)
-
+    await adminPage.unroutePersonnelRequest()
 })
